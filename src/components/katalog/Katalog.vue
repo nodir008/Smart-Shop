72978d472e9abfb9982f9c14f788140883b2b7ed
shohakimovv <alisher>
2024-03-24 12:59:59 +0500

    togirlandi

--- src/components/katalog/Katalog.vue
+++ src/components/katalog/Katalog.vue
@@ -18,16 +18,16 @@ const closeKatalogAndLoadCategory = (title) => {
 </script>
 
 <template>
-    <div class="katalog">
-        <h3 class="katalog-title">{{ $t("nav__category-title") }}</h3>
-        <RouterLink
-            v-for="title in productsCategories.categorie"
-            :to="'/category/' + title"
-            class="nav__category-page"
-            :key="title.id"
-            @click="closeKatalogAndLoadCategory(title)"
-        >
-            {{ title }}
-        </RouterLink>
-    </div>
+        <div class="katalog">
+            <h3 class="katalog-title">{{ $t("nav__category-title") }}</h3>
+            <RouterLink
+                v-for="title in productsCategories.categorie"
+                :to="'/category/' + title"
+                class="nav__category-page"
+                :key="title.id"
+                @click="closeKatalogAndLoadCategory(title)"
+            >
+                {{ title }}
+            </RouterLink>
+        </div> 
 </template>
