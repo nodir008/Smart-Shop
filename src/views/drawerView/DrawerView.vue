e7a487dd92e54516a7a0f9b7deb50631e587de77
shohakimovv <alisher>
2024-04-12 17:34:26 +0500

    media toggke

--- src/views/drawerView/DrawerView.vue
+++ src/views/drawerView/DrawerView.vue
@@ -229,26 +229,13 @@ const toggleCheckboxAdd = () => {
                                         </div>
                                     </div>
                                     </div>
-                                    <div class="checkbox-wrapper-12">
-                                        <div class="cbx">
-                                            <input type="checkbox" v-model="item.isdrawerForActive" />
-    
-                                            <label for="cbx-12"></label>
-                                            <svg fill="none" viewBox="0 0 15 14" height="14" width="15">
-                                                <path d="M2 8.36364L6.23077 12L13 2"></path>
-                                            </svg>
-                                        </div>
-    
-                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                            <defs>
-                                                <filter id="goo-12">
-                                                    <feGaussianBlur result="blur" stdDeviation="4" in="SourceGraphic"></feGaussianBlur>
-                                                    <feColorMatrix result="goo-12" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" mode="matrix" in="blur"></feColorMatrix>
-                                                    <feBlend in2="goo-12" in="SourceGraphic"></feBlend>
-                                                </filter>
-                                            </defs>
-                                        </svg>
-                                    </div>
+                                    <div class="checkbox-wrapper">
+                                        <label>
+                                          <input type="checkbox" v-model="item.isdrawerForActive" >
+                                          <span class="checkbox"></span>
+                                        </label>
+                                      </div>
+                                    <!-- <input type="checkbox" v-model="item.isdrawerForActive" /> -->
                                 </div>
                                 <div class="drawer__card-none-div">
                                     <div class="drawer__card-none-count-div">
